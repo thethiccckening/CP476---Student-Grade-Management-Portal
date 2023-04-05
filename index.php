88e0d1d3b32ced885f7b57ad93fbb65f71780cb8
Sekou Diaby <diabilasekou@gmail.com>
2023-04-05 16:53:08 -0400

    final touches

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Lala Land Student Management Portal</title>
+  <title>Student Management Portal</title>
   <link rel="stylesheet" href="styles.css">
   <script src="script.js" defer></script>
 </head>
 <body>
-  <h1 style="text-align: center;">Lala Land Student Management Portal</h1>
+  <h1 style="text-align: center;">Student Management Portal</h1>
 
 
 <div class="button-container">
